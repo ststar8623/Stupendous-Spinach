828075c567b007a45caac02fb68eae93b5e6f8b5
Joseph Lei <josephlei@Josephs-MacBook-Pro.local>
2017-07-18 09:30:04 -0700

    camera

---
+++ client/src/components/Camera.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class Camera extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+
+      <div>
+        <h1> Camera page</h1>
+      </div>
+
+      );
+  }
+
+
+}
+
+
+export default Camera;
