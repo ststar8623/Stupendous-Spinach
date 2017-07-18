8aa1dc0bbfb45fdc8f3abfb59dddaff05afb4f8e
Paul Dockery <paul.david.dockery@gmail.com>
2017-07-18 14:48:15 -0700

    (bug) remove run-service-worker (incorrect reference)

--- public/run-service-worker.js
+++
@@ -1,8 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register(__dirname + '../service-worker.js')
-      .then(() => {
-        console.log('Service Worker Registered');
-      });
-  });
-}
