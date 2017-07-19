d18de74042774c49578bc5f4b6567d320fc5ad14
Joseph Lei <josephlei@Josephs-MacBook-Pro.local>
2017-07-18 18:14:09 -0700

    add development

--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@
 const app = require('./app');
 const db = require('../db');
 const config = require('config')['knex'];
+const config2 = require('config')['passport'];
 const PORT = process.env.PORT || 3000;
 
 console.log('the current environment is: ', app.get('env'));
+console.log('the current environment is: ', config2["Facebook"]["clientID"]);
+
 
 
 app.listen(PORT, () => {
