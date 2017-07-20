dbe78898b7fe968e4f2378399f3206a4c56643ec
Steven Leung <ststar8623@gmail.com>
2017-07-20 13:54:50 -0700

    Update NearbyPhotoCard.js

--- client/src/components/NearbyPhotoCard.js
+++ client/src/components/NearbyPhotoCard.js
@@ -9,14 +9,24 @@ class NearbyPhotoCard extends Component {
     return (
       <div className="img-rounded">
         <img src={this.props.photo.url} height={200} width ={300} className='.img-thumbnail'/>
-        <div id='like'>
+        <div style={styles.like}>
           <span className="fa fa-heart" aria-hidden="true"> Likes </span>
-          <span className="fa fa-comment" id="comments" aria-hidden="true"> Comments </span>
+          <span className="fa fa-comment" style={styles.comment} aria-hidden="true"> Comments </span>
         </div>
         <h6 className='text'>{this.props.photo.caption} </h6>
       </div>
     );
   }
 }
+
+const styles = {
+  like: {
+    fontSize: '16px',
+    padding: '4px'
+  },
+  comment: {
+    float: 'right'
+  }
+};
 
 export default NearbyPhotoCard;
