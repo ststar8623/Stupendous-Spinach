fa2ce86857b794dd988d1c4b85fedfddc3292c93
Paul Dockery <paul.david.dockery@gmail.com>
2017-07-21 15:36:09 -0700

    incomplete wiring for new image upload process

--- client/src/components/PreviewAndShare.js
+++ client/src/components/PreviewAndShare.js
@@ -11,14 +11,27 @@ class PreviewAndShare extends Component {
     // };
     this.state = {
       shareSelection: 'everyone',
-      commentText: '',
-      photo: 'http://en.protothema.gr/wp-content/uploads/2016/01/anamur.jpg.pagespeed.ce_.y8U5lThvvI.jpg',
-      id: 1
+      captionText: '',
+      photo: '',
     };
 
+    // {
+    //   id: 1,
+    //   profile_id: 1,
+    //   latitude: '37.7876° N',
+    //   longitude: '122.4001° W',
+    //   url: 'http://img07.deviantart.net/a85d/i/2013/022/0/3/san_francisco_city_by_tt83x-d5seu41.jpg',
+    //   like_count: 1,
+    //   comment_count: 1,
+    //   caption: 'SF at night!' 
+    // }
+// Does the image object live in state/store prior to imageupload in camera?
+// if yes, need to delay submission of image until this page, after adding caption and share field
+  
+
+    this.handleCaptionSubmit = this.handleCaptionSubmit.bind(this);
     this.handleCaptionChange = this.handleCaptionChange.bind(this);
     this.handleShareChange = this.handleShareChange.bind(this);
-    // this.photo.url = 'http://en.protothema.gr/wp-content/uploads/2016/01/anamur.jpg.pagespeed.ce_.y8U5lThvvI.jpg';
   }
 
   handleCaptionChange(e) {
@@ -33,6 +46,17 @@ class PreviewAndShare extends Component {
     });
   }
 
+  handleCaptionSubmit(e) {
+    e.preventDefault();
+    console.log('handleCaptionChange invoked');
+    let that = this;
+    imageUpload(file, data => {
+      that.props.imageStoreAction(data);
+      browserHistory.push('/PreviewAndShare'); 
+    });
+ 
+  }
+
   render() {
     console.log('propsssssssss ', this.props);
     return (
@@ -40,7 +64,7 @@ class PreviewAndShare extends Component {
         <div>
           <img src={this.props.url} height={200} width ={300} className='.img-thumbnail'/>
         </div>
-        <form className="photo-form">
+        <form className="photo-form" onSubmit={this.handleCaptionSubmit}>
           <ul>
             <li style={ styles.li }><input type="radio" name="share-selection" value="everyone" onChange={this.handleShareChange} checked={this.state.shareSelection === 'everyone'} />Share with everyone</li>
             <li style={ styles.li }><input type="radio" name="share-selection" value="friends" onChange={this.handleShareChange} checked={this.state.shareSelection === 'friends'} />Share with friends only</li>
@@ -61,9 +85,9 @@ const styles = {
 
 const mapStateToProps = (state) => {
   return {
-    url: state.upload.url
+    url: state.upload.url,
+    location: state.location
   };
 };
 
 export default connect(mapStateToProps)(PreviewAndShare);
-// export default connect(mapStateToProps, {getLocation})(Camera);
