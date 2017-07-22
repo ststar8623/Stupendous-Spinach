25c7ead772e461e14fe6e868bfac4f7823c74835
ststar8623 <ststar8623@gmail.com>
2017-07-21 23:59:49 -0700

    Finish Likes button

--- client/src/components/NearbyPhotoCard.js
+++ client/src/components/NearbyPhotoCard.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import Comments from './Comments';
 import { Link } from 'react-router';
-import axios from 'axios';
 import { connect } from 'react-redux';
 import { increment, decrement } from '../actions/likeAction';
 import { bindActionCreators } from 'redux';
+import { axiosAction } from '../helpers/axiosAction';
 
 require('../styles/main.css');
 
@@ -13,19 +13,28 @@ class NearbyPhotoCard extends Component {
     super(props);
   }
 
+  likeOrDislike(i, liked, id) {
+    const likeOrDislike = liked ? 'decrement' : 'increment';
+    const putOrPost = liked ? 'put' : 'post';
+    const axiosLikeOrDislike = liked ? `/api/removelike/${id}` : `/api/addlike/${id}`;
+
+    this.props[likeOrDislike](i);
+    axiosAction(putOrPost, axiosLikeOrDislike, null, (response) => {
+      console.log('successfully from the database', response);
+    });
+  }
+
   render() {
     const { url, like_count, comment_count, id, caption, liked } = this.props.photo;
     const commentId = `/comments/${id}`;
     const { i } = this.props;
-
     const heart = liked ? "fa fa-heart heart" : "glyphicon glyphicon-heart-empty heart";
-    const likeOrDislike = liked ? 'decrement' : 'increment';
 
     return (
       <div className="img-rounded">
         <img src={ url } className='img-thumbnail'/>
         <div>
-          <span className={ heart } aria-hidden="true" onClick={ this.props[likeOrDislike].bind(null, i) }>{ like_count }</span>
+          <span className={ heart } aria-hidden="true" onClick={ this.likeOrDislike.bind(this, i, liked, id) }>{ like_count }</span>
           <span className="fa fa-comment comment" aria-hidden="true">
             <Link to={ commentId }>{ comment_count }<span className="comments">Comments</span></Link>
           </span>
