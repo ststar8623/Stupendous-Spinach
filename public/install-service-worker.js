fbd7d2236f6d3bdc6ddc119c218dbc96922a2099
Paul Dockery <paul.david.dockery@gmail.com>
2017-07-25 15:35:14 -0700

    log whether SWs allowed in client interpreter

--- public/install-service-worker.js
+++ public/install-service-worker.js
@@ -1,5 +1,9 @@
+console.log('Service Worker available in navigator?: ', 'serviceWorker' in navigator);
+
 if ('serviceWorker' in navigator) {
+  console.log('navigator: ', navigator);
   // window.addEventListener('load', () => {
+  
   navigator.serviceWorker.register('service-worker.js')
   .then(() => {
     console.log('Service Worker Registered');
