49391afc4d461d43e61226a6d7d68e7b3511bae7
Paul Dockery <paul.david.dockery@gmail.com>
2017-07-27 16:20:34 -0700

    (refactor) linting adjustment

--- public/install-service-worker.js
+++ public/install-service-worker.js
@@ -5,7 +5,7 @@ if ('serviceWorker' in navigator) {
   // window.addEventListener('load', () => {
   
   navigator.serviceWorker.register('service-worker.js')
-  .then(() => {
-    console.log('Service Worker Registered');
-  });
+    .then(() => {
+      console.log('Service Worker Registered');
+    });
 }
