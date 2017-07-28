a28a8fc0f303945c7df1fc0d7fcd215b73e0758f
sara3 <sadaqiq@gmail.com>
2017-07-27 19:05:55 -0700

    added end point for profile map

--- db/models/photos.js
+++ db/models/photos.js
@@ -17,19 +17,32 @@ class PhotoQueries {
     return knex.raw(query);
   }
 
-  getPhotos(numOfPhotos) {
+  getPhotos(numOfPhotos, userID) {
+
+    console.log("num of photos", numOfPhotos);
+    console.log("userID", userID);
 
     let query;
 
     if (numOfPhotos) {
+      //run this if num of photos is not undefined
+
       query = `select pro.first, p.*, age(current_date + current_time, p.created_at) as "age", pro.photo as "profile_photo" from photos p join profiles pro on p.profile_id = pro.id order by created_at desc limit ${numOfPhotos}`;
+    } else if (userID) {
+      //run this if num of photos is undefined and user id is undefined
+      query = `select pro.first, p.*, age(current_date + current_time, p.created_at) as "age", pro.photo as "profile_photo" from photos p join profiles pro on p.profile_id = pro.id where p.profile_id = ${userID} order by created_at desc`;
     } else {
+      //run this if numof photos is undefined and user id is defined
       query = `select pro.first, p.*, age(current_date + current_time, p.created_at) as "age", pro.photo as "profile_photo" from photos p join profiles pro on p.profile_id = pro.id order by created_at desc`;
+
+
+     
     }
+
+    console.log(query);
 
     return knex.raw(query);
   }
-
 }
 
 
