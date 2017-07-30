f28a38b70872f6418cac574aa97abca6b857f04b
Paul Dockery <paul.david.dockery@gmail.com>
2017-07-29 17:28:13 -0700

    (cleanup) comment logging

--- public/service-worker.js
+++ public/service-worker.js
@@ -62,7 +62,6 @@ var urlsToIgnore = shellFilesToCache.concat([
 ]);
 
 
-
 // Install SW and cache shell files
 self.addEventListener('install', (e) => {
   e.waitUntil(
@@ -182,83 +181,15 @@ const cachedShellResponse = function(request) {
 //   console.log(info.usage); // Result: <used data in bytes>
 // });
 
+// if (urlsToIgnore.every(urlBit => (e.request.url.indexOf(urlBit) === -1))) {
 
-/* SW-Cache example
+// } else if (!e.request.headers.has('Authorization')) {
 
-'use strict';
-
-if ('serviceWorker' in navigator) {
-  // Delay registration until after the page has loaded, to ensure that our
-  // precaching requests don't degrade the first visit experience.
-  // See https://developers.google.com/web/fundamentals/instant-and-offline/service-worker/registration
-  window.addEventListener('load', function() {
-    // Your service-worker.js *must* be located at the top-level directory relative to your site.
-    // It won't be able to control pages unless it's located at the same level or higher than them.
-    // *Don't* register service worker file in, e.g., a scripts/ sub-directory!
-    // See https://github.com/slightlyoff/ServiceWorker/issues/468
-    navigator.serviceWorker.register('service-worker.js').then(function(reg) {
-      // updatefound is fired if service-worker.js changes.
-      reg.onupdatefound = function() {
-        // The updatefound event implies that reg.installing is set; see
-        // https://w3c.github.io/ServiceWorker/#service-worker-registration-updatefound-event
-        var installingWorker = reg.installing;
-
-        installingWorker.onstatechange = function() {
-          switch (installingWorker.state) {
-            case 'installed':
-              if (navigator.serviceWorker.controller) {
-                // At this point, the old content will have been purged and the fresh content will
-                // have been added to the cache.
-                // It's the perfect time to display a "New content is available; please refresh."
-                // message in the page's interface.
-                console.log('New or updated content is available.');
-              } else {
-                // At this point, everything has been precached.
-                // It's the perfect time to display a "Content is cached for offline use." message.
-                console.log('Content is now available offline!');
-              }
-              break;
-
-            case 'redundant':
-              console.error('The installing service worker became redundant.');
-              break;
-          }
-        };
-      };
-    }).catch(function(e) {
-      console.error('Error during service worker registration:', e);
-    });
-  });
-}
-*/
-
-
-// } else if (shellFilesToCache.every(urlBit => (e.request.url.indexOf(urlBit) === -1))) {
-// caches.open(userDataCacheName).then((cache) => {
-//   // console.log('[ServiceWorker] User Data caching pre-fetch\nurl: ', e.request.url);
-//   openCache = cache;
-//   return fetch(e.request); // {credentials include} arg needed?
-// }).then((response) => {
-//   // console.log('[ServiceWorker] User Data caching post-fetch\nurl: ', e.request.url);
-//   openCache.put(e.request.url, response.clone());
-//   return response;
-// }).catch((error) => {
-//   console.log('[ServiceWorker] ERROR serverApi: ', error, '\nurl: ', e.request.url);
-// })
-
-
-  // if (urlsToIgnore.every(urlBit => (e.request.url.indexOf(urlBit) === -1))) {
-
-
-  // } else if (!e.request.headers.has('Authorization')) {
-
-
-
-  // else {
-  //   // console.log('[Service Worker] ignored URL: ', e.request.url);
-  //   e.respondWith(
-  //     caches.match(e.request).then((response) => {
-  //       return response || fetch(e.request);
-  //     })
-  //   );
-  // }
+// else {
+//   // console.log('[Service Worker] ignored URL: ', e.request.url);
+//   e.respondWith(
+//     caches.match(e.request).then((response) => {
+//       return response || fetch(e.request);
+//     })
+//   );
+// }
