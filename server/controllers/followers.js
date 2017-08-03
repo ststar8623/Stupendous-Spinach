9cf158da72cf168cf9f015353312081973445172
Joe Lei <jlei523@gmail.com>
2017-08-03 11:27:47 -0700

    finish follower endpoint

--- server/controllers/followers.js
+++ server/controllers/followers.js
@@ -3,17 +3,34 @@ const utils = require('./lib/utils.js');
 
 module.exports.addFollower = (userID, followerID) => {
 
- return models.Follower.FollowersQueries.addFollower(userID, followerID)
-    .then((data) => {
-      return models.Follower.FollowersQueries.incrementFollowerCount(userID);
-    })
-    .then((data) => {
-      return models.Follower.FollowersQueries.incrementFollowingCount(followerID);
-    });
+  return models.Follower.FollowersQueries.checkIfFollowed(userID, followerID)
+  .then((data) => {
+    console.log('data from check if followed', data.rows);
+    
+    return data.rows.length > 0;
+
+  })
+  .then((data) => {
+    if (data) {
+      return models.Follower.FollowersQueries.addFollower(userID, followerID)
+        .then((data) => {
+          return models.Follower.FollowersQueries.incrementFollowerCount(userID);
+        })
+        .then((data) => {
+          return models.Follower.FollowersQueries.incrementFollowingCount(followerID);
+        });
+
+
+    } else {
+      return { data: 'You already followed this user'};
+    }
+
+  });
 
 };
 
-module.exports.getFollowers = (userID) => {
+module.exports.removeFollower = (userID, followerID) => {
   
+  return models.Follower.FollowersQueries.removeFollower(userID, followerID);
  
 };
